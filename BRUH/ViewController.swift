3f8eebd148a17de3c8d96690a6d51d580dfcaeb8
Shanese Scott <shanesescott@Shaneses-MacBook-Pro.local>
2016-11-07 20:56:26 -0500

    Initial Commit

---
+++ BRUH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BRUH
+//
+//  Created by Shanese Scott on 11/7/16.
+//  Copyright © 2016 Shanese Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
